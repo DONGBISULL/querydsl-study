bd39fb54d012837f6e87a8cbecb33da4660d33a2
SDB <dbsull@plani.co.kr>
2023-05-04 16:26:22 +0900

    ## application 파일 수정 / 로그 관련 의존성 추가

---
+++ src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,47 @@
+package study.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter // 실무에선 사용에 주의
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자를 만들어줌
+@ToString(of = {"id", "username", "age"}) // 자신이 소유한 필드에 대해서만 toString 을 만들어야 함 그외 것들을 가지고 있을 경우 순환 오류 !!
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String username;
+
+    private int age;
+
+    // 연관관계 주인!
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id") //외래키 이름 빠트린 부분이므로 꼭 내용 확인
+    private Team team;
+
+    public Member(String username) {
+        this(username, 0);
+    }
+
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,65 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+    @Autowired
+    EntityManager em;
+
+    @BeforeEach
+    public void before() {
+        Team teamA = new Team("TeamA");
+        Team teamB = new Team("TeamB");
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamA);
+
+        Member member3 = new Member("member3", 10, teamB);
+        Member member4 = new Member("member4", 10, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL() {
+        //member1을 찾아라
+        String qlString = "select m from Member m where m.username = :username"; // 메서드가 실행됐을 때 에러 발생
+        Member findMember = em.createQuery(qlString, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl() {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QMember m = new QMember("m");
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+}
